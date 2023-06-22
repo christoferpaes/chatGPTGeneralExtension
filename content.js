a3998c736df65ee4ccf9a1848d5ef1a8b2b59fba
Christofer Patrick Paes <49449786+christoferpaes@users.noreply.github.com>
2023-06-22 12:37:29 -0700

    Update content.js

--- content.js
+++ content.js
@@ -1,29 +1,49 @@
-function extractHTMLContent() {
-  return document.documentElement.outerHTML;
+// content.js
+
+// Function to extract the data from the webpage
+function extractDataFromWebpage() {
+  // Add your parsing logic here to extract the desired data from the webpage
+  // Example:
+  const dataElement = document.querySelector('.data-selector');
+  const data = dataElement.innerText;
+  return data;
 }
 
-function sendMessageToBackgroundScript(message, data, callback) {
-  chrome.runtime.sendMessage({ message, data }, response => {
-    if (chrome.runtime.lastError) {
-      console.error(chrome.runtime.lastError);
-      callback("");
-    } else {
-      callback(response);
-    }
-  });
-}
-
+// Function to handle the response from ChatGPT
 function handleChatGPTResponse(response) {
-  const generatedText = response;
-  console.log("Generated Text:", generatedText);
+  // Process the response from ChatGPT and perform any necessary actions
+  // Example:
+  const generatedText = response.choices[0].text;
+  console.log('Generated Text:', generatedText);
   // Perform further actions with the generated text as needed
 }
 
+// Main function to initiate the process
 function processWebpage() {
-  const htmlContent = extractHTMLContent();
+  const data = extractDataFromWebpage();
 
-  sendMessageToBackgroundScript("html_content", htmlContent, handleChatGPTResponse);
+  // Use the extracted data as input for ChatGPT API
+  // Example:
+  const apiUrl = 'https://api.openai.com/v1/engines/davinci-codex/completions';
+  const prompt = `Given the data: ${data}`;
+  const dataToSend = {
+    prompt,
+    max_tokens: 50
+  };
+
+  // Send a request to the ChatGPT API
+  fetch(apiUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer YOUR_API_KEY'  // Replace with your actual API key
+    },
+    body: JSON.stringify(dataToSend)
+  })
+    .then(response => response.json())
+    .then(handleChatGPTResponse)
+    .catch(error => console.error('Error:', error));
 }
 
+// Call the main function to start the process when the webpage is loaded
 processWebpage();
-

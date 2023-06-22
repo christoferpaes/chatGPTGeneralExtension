5e72954013e703c6ac0b3c41457bf90d75e4f848
Christofer Patrick Paes <49449786+christoferpaes@users.noreply.github.com>
2023-06-22 12:10:03 -0700

    Update background.js

--- background.js
+++ background.js
@@ -34,3 +34,45 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     handleHTMLCode(request.htmlCode);
   }
 });
+// background.js
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.message === 'html_content') {
+    // Send the HTML content to ChatGPT and handle the response
+    sendToChatGPT(request.content, function (response) {
+      // Send the response back to the content script
+      sendResponse({ message: 'chat_response', response: response });
+    });
+
+    // Return true to indicate that the response will be sent asynchronously
+    return true;
+  }
+});
+
+function sendToChatGPT(htmlContent, callback) {
+  // Make the API request to ChatGPT using the HTML content
+  // Replace 'YOUR_OPENAI_API_KEY' with your actual OpenAI API key
+  const apiKey = 'YOUR_OPENAI_API_KEY';
+  const apiUrl = 'https://api.openai.com/v1/engines/davinci-codex/completions';
+
+  fetch(apiUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${apiKey}`
+    },
+    body: JSON.stringify({
+      prompt: htmlContent,
+      max_tokens: 50
+    })
+  })
+    .then(response => response.json())
+    .then(data => {
+      const chatResponse = data.choices[0].text.trim();
+      callback(chatResponse);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      callback('An error occurred while processing the request.');
+    });
+}
